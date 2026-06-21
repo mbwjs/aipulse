32b257c5da2b790ad9dc2e3cd4dea4696dba6717
Bowen Ma <mab073592@gmail.com>
2026-06-22 00:59:31 +0800

    Add GitHub link to nav, ignore deploy.sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-echo "Building Hugo site..."
-hugo --minify
-echo "Deploying to aipulse.lol..."
-rsync -avz --delete public/ root@aipulse.lol:/var/www/aitracker/
-echo "Done! https://aipulse.lol/"
